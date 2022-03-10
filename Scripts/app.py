55d5b18eb837dba534cfc0647747037d67493894
JuanseMeli1608 <juacorredor@mercadolibre.com.co>
2022-03-10 10:57:51 -0500

    Fixing none type in uploaded file and on change action on prediction type

--- Scripts/app.py
+++ Scripts/app.py
@@ -23,6 +23,8 @@ def uploaded_file_change():
     st.session_state.uploaded_file_ = True
     st.session_state.predict_button_ = False
     st.session_state.img_id_ += 1
+def predict_type_change():
+    st.session_state.predict_button_ = False
 def send_feedback_button_click():
     st.session_state.send_feedback_button_ = True
 def init_ss():
@@ -68,41 +70,42 @@ img_descriptor = load_model('Data/Objects/parameters.json', 'Data/Model/', 'Data
 uploaded_file = st.file_uploader(label="Upload Image", type=["png", "jpeg", "jpg"], key='uploaded_file', on_change=uploaded_file_change)
 if st.session_state.uploaded_file_:
     # Read and save image
-    st.session_state.uploaded_image = uploaded_file.read()
-    st.image(st.session_state.uploaded_image, use_column_width=True)
+    if st.session_state.uploaded_image is not None:
+        st.session_state.uploaded_image = uploaded_file.read()
+        st.image(st.session_state.uploaded_image, use_column_width=True)
 
-    # Prediction
-    st.subheader('Prediction Parameters')
-    col1, _, _ = st.columns(3)
-    predict_type = col1.selectbox("Select Prediction Type", ("Argmax", "Probabilistic"), help='''__Argmax__ picks the value/token with the highest probability.
-    __Probabilistic__ picks the value/token randomly using the distribution of the predictions.''', key='predict_type')
-    predict_button = st.button('Predict Caption', key='predict_button', on_click=predict_button_click)
-    if st.session_state.predict_button or st.session_state.predict_button_:
-        img, _ = img_descriptor.preprocess(st.session_state.uploaded_image)
-        result, caption, attention_plot, _ = img_descriptor.predict(img, predict_type)
-        st.subheader(caption + '.')
-        fig = img_descriptor.plot_attention(result=result, attention_plot=attention_plot, path=st.session_state.uploaded_image)
-        st.subheader('Neural Network Attention Word by Word')
-        st.pyplot(fig)
+        # Prediction
+        st.subheader('Prediction Parameters')
+        col1, _, _ = st.columns(3)
+        predict_type = col1.selectbox("Select Prediction Type", ("Argmax", "Probabilistic"), help='''__Argmax__ picks the value/token with the highest probability.
+        __Probabilistic__ picks the value/token randomly using the distribution of the predictions.''', key='predict_type', on_change=predict_type_change)
+        predict_button = st.button('Predict Caption', key='predict_button', on_click=predict_button_click)
+        if st.session_state.predict_button or st.session_state.predict_button_:
+            img, _ = img_descriptor.preprocess(st.session_state.uploaded_image)
+            result, caption, attention_plot, _ = img_descriptor.predict(img, predict_type)
+            st.subheader(caption + '.')
+            fig = img_descriptor.plot_attention(result=result, attention_plot=attention_plot, path=st.session_state.uploaded_image)
+            st.subheader('Neural Network Attention Word by Word')
+            st.pyplot(fig)
 
-        # Feedback
-        st.session_state.feedback = SEND_FEEDBACK
-        if st.session_state.feedback:
-            st.subheader('Feedback')
-            tag_caption = st.selectbox("Is this correct?", ("Select an option", "Yes", "No"), key='tag_caption', on_change=tag_caption_change)
-            if st.session_state.tag_caption_:
-                if tag_caption != 'Select an option':
-                    if tag_caption == 'Yes':
-                        correct_caption = caption
-                    elif tag_caption == 'No':
-                        correct_caption = st.text_input("What should the correct caption be?", "", key='correct_caption')
-                    send_feedback_button = st.button('Send Feedback', key='send_feedback_button', on_click=send_feedback_button_click)
-                    if st.session_state.send_feedback_button_:
-                        gcp_user.load_image_and_caption(
-                            bigquery_table_name = 'img-captioning-322620.feedbacks.CORRECT_CAPTIONS',
-                            caption = correct_caption,
-                            feedback_query = 'Data/Feedback/feedbacks_new_id.sql',
-                            storage_bucket_name = 'img-captioning-feedback',
-                            uploaded_image = st.session_state.uploaded_image
-                        )
-                        st.write("Thank you for that, we'll use your help to make our model better!")
+            # Feedback
+            st.session_state.feedback = SEND_FEEDBACK
+            if st.session_state.feedback:
+                st.subheader('Feedback')
+                tag_caption = st.selectbox("Is this correct?", ("Select an option", "Yes", "No"), key='tag_caption', on_change=tag_caption_change)
+                if st.session_state.tag_caption_:
+                    if tag_caption != 'Select an option':
+                        if tag_caption == 'Yes':
+                            correct_caption = caption
+                        elif tag_caption == 'No':
+                            correct_caption = st.text_input("What should the correct caption be?", "", key='correct_caption')
+                        send_feedback_button = st.button('Send Feedback', key='send_feedback_button', on_click=send_feedback_button_click)
+                        if st.session_state.send_feedback_button_:
+                            gcp_user.load_image_and_caption(
+                                bigquery_table_name = 'img-captioning-322620.feedbacks.CORRECT_CAPTIONS',
+                                caption = correct_caption,
+                                feedback_query = 'Data/Feedback/feedbacks_new_id.sql',
+                                storage_bucket_name = 'img-captioning-feedback',
+                                uploaded_image = st.session_state.uploaded_image
+                            )
+                            st.write("Thank you for that, we'll use your help to make our model better!")
